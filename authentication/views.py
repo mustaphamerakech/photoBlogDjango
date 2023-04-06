cb0be5cb967e1e96ff51cc694f74f72bbf653b5c
mustapha <mustamerak01@gmail.com>
2023-04-06 18:34:15 +0100

    register

--- authentication/views.py
+++ authentication/views.py
@@ -1,52 +1,28 @@
-from django.contrib.auth import  authenticate, login, logout# import des fonctions login et authenticate
-from django.shortcuts import render, redirect
+from telnetlib import LOGOUT
+from django.conf import settings
+from django.contrib.auth import login
+from django.shortcuts import redirect, render
+
 from . import forms
 
-from django.views.generic import View
 
-
-class LoginPageView(View):
-    template_name = 'authentication/login.html'
-    form_class = forms.LoginForm
-
-    def get(self, request):
-        form = self.form_class()
-        message = ''
-        return render(request, self.template_name, context={'form': form, 'message': message})
-        
-    def post(self, request):
-        form = self.form_class(request.POST)
+def signup_page(request):
+    form = forms.SignupForm()
+    if request.method == 'POST':
+        form = forms.SignupForm(request.POST)
         if form.is_valid():
-            user = authenticate(
-                username=form.cleaned_data['username'],
-                password=form.cleaned_data['password'],
-            )
-            if user is not None:
-                login(request, user)
-                return redirect('home')
-        message = 'Identifiants invalides.'
-        return render(request, self.template_name, context={'form': form, 'message': message})
+            user = form.save()
+            # auto-login user
+            login(request, user)
+            return redirect(settings.LOGIN_REDIRECT_URL)
+    return render(request, 'authentication/signup.html', context={'form': form})
+# S3cret!
+
+
 
 def logout_user(request):
-    
-    logout(request)
-    return redirect('login')
-    
 
-def login_page(request):
-    form = forms.LoginForm()
-    message = ''
-    if request.method == 'POST':
-        form = forms.LoginForm(request.POST)
-        if form.is_valid():
-            user = authenticate(
-                username=form.cleaned_data['username'],
-                password=form.cleaned_data['password'],
-            )
-            if user is not None:
-                login(request, user)
-                return redirect("home")
-            else:
-                message = 'Identifiants invalides.'
-    return render(
-        request, 'authentication/login.html', context={'form': form, 'message': message})
+    LOGOUT(request)
+    return redirect('login')
+
+
--- photoblog/urls.py
+++ photoblog/urls.py
@@ -16,17 +16,20 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import (
+    LoginView, LogoutView, PasswordChangeView, PasswordChangeDoneView)
 import authentication.views
 import blog.views
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('', LoginView.as_view(
             template_name='authentication/login.html',
             redirect_authenticated_user=True), name='login'),
     path('logout/', authentication.views.logout_user, name='logout'),
     path('home/', blog.views.home, name='home'),
+    path('signup/', authentication.views.signup_page, name='signup'),
     
 ]
