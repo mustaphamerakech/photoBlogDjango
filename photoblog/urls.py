723ad42fa8d928671ddd9e871fd463b86d5bb4c8
mustapha <mustamerak01@gmail.com>
2023-04-08 14:18:49 +0100

    add photo

--- photoblog/urls.py
+++ photoblog/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.contrib.auth.views import (
@@ -31,5 +33,8 @@ urlpatterns = [
 
     path('home/', blog.views.home, name='home'),
     path('signup/', authentication.views.signup_page, name='signup'),
-    
+    path('photo/upload/', blog.views.photo_upload, name='photo_upload'),
 ]
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
