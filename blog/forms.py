d5f84e2f8debca2e3b29fac460febd755f86b8c5
mustapha <mustamerak01@gmail.com>
2023-04-11 17:09:59 +0100

    change blog

--- blog/forms.py
+++ blog/forms.py
@@ -4,7 +4,6 @@ from . import models
 
 
 class PhotoForm(forms.ModelForm):
-
     class Meta:
         model = models.Photo
         fields = ['image', 'caption']
@@ -12,8 +11,11 @@ class PhotoForm(forms.ModelForm):
 
 class BlogForm(forms.ModelForm):
     edit_blog = forms.BooleanField(widget=forms.HiddenInput, initial=True)
+
     class Meta:
         model = models.Blog
         fields = ['title', 'content']
+
+
 class DeleteBlogForm(forms.Form):
     delete_blog = forms.BooleanField(widget=forms.HiddenInput, initial=True)
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.forms import formset_factory
 from django.shortcuts import get_object_or_404, redirect, render
 
 from . import forms, models
@@ -41,6 +42,7 @@ def blog_and_photo_upload(request):
             blog.author = request.user
             blog.photo = photo
             blog.save()
+            return redirect('home')
     context = {
         'blog_form': blog_form,
         'photo_form': photo_form,
@@ -53,6 +55,7 @@ def view_blog(request, blog_id):
     blog = get_object_or_404(models.Blog, id=blog_id)
     return render(request, 'blog/view_blog.html', {'blog': blog})
 
+
 @login_required
 def edit_blog(request, blog_id):
     blog = get_object_or_404(models.Blog, id=blog_id)
@@ -64,13 +67,28 @@ def edit_blog(request, blog_id):
             if edit_form.is_valid():
                 edit_form.save()
                 return redirect('home')
-            if 'delete_blog' in request.POST:
-                delete_form = forms.DeleteBlogForm(request.POST)
-                if delete_form.is_valid():
-                    blog.delete()
-                    return redirect('home')
+        if 'delete_blog' in request.POST:
+            delete_form = forms.DeleteBlogForm(request.POST)
+            if delete_form.is_valid():
+                blog.delete()
+                return redirect('home')
     context = {
         'edit_form': edit_form,
         'delete_form': delete_form,
     }
     return render(request, 'blog/edit_blog.html', context=context)
+
+
+def create_multiple_photos(request):
+    PhotoFormSet = formset_factory(forms.PhotoForm, extra=5)
+    formset = PhotoFormSet()
+    if request.method == 'POST':
+        formset = PhotoFormSet(request.POST, request.FILES)
+        if formset.is_valid():
+            for form in formset:
+                if form.cleaned_data:
+                    photo = form.save(commit=False)
+                    photo.uploader = request.user
+                    photo.save()
+            return redirect('home')
+    return render(request, 'blog/create_multiple_photos.html', {'formset': formset})
